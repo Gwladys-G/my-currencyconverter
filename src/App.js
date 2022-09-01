9407f78693e0a07c2589b3bbc83e365fcee3331d
Gwladys Engels <gwladys.engels@gmail.com>
2022-09-01 17:01:30 -0400

    simple converter logic

--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import CurrencyRow from './CurrencyRow';
+import './index.css';
+import { useEffect, useState } from 'react';
+
+
+const MYHEADERS = new Headers();
+MYHEADERS.append("apikey", "pyUStTLtFF0BmM09n7CRyeZRkdS9xkY8");
+
+var requestOptions = {
+  method: 'GET',
+  redirect: 'follow',
+  headers: MYHEADERS
+};
+
 
 function App() {
+
+  const [currencyOptions, setCurrencyOptions] = useState([])
+  const [fromCurrency, setFromCurrency] = useState("EUR")
+  const [toCurrency, setToCurrency ] = useState("CAD")
+  const [exchangeRate, setExchangeRate] = useState()
+  const [amount, setAmount ] = useState(1)
+  const [amountInFromCurrency, setAmountInFromCurrency] = useState(true)
+  console.log(exchangeRate)
+
+  let toAmount,fromAmount
+  if (amountInFromCurrency){
+    fromAmount = amount
+    toAmount = amount * exchangeRate
+  } else {
+    toAmount = amount
+    fromAmount = amount /exchangeRate
+  }
+
+  useEffect( () => {
+    fetch(`https://api.apilayer.com/exchangerates_data/latest?base=${fromCurrency}&symbols=CAD,AUD,EUR,JPY,USD`, requestOptions)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data)
+        setCurrencyOptions([...Object.keys(data.rates)])
+        console.log(toCurrency)
+        setExchangeRate(data.rates[toCurrency])
+      })
+      .catch(error => console.log('error', error));
+  },[fromCurrency,toCurrency])
+
+  const handleFromAmountChange = (e) => {
+    setAmount(e.target.value)
+    setAmountInFromCurrency(true)
+  }
+  const handleToAmountChange = (e) => {
+    setAmount(e.target.value)
+    setAmountInFromCurrency(false)
+  }
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Convert</h1>
+      <CurrencyRow currencyOptions={currencyOptions} selectedCurrency={fromCurrency} onChangeCurrency={(e) => {setFromCurrency(e.target.value)}} amount={fromAmount} onChangeAmount={handleFromAmountChange}/>
+      <div className='equals'>=</div>
+      <CurrencyRow currencyOptions={currencyOptions} selectedCurrency={toCurrency} onChangeCurrency={(e) => {setToCurrency(e.target.value)}} amount={toAmount} onChangeAmount={handleToAmountChange}/>
+    </>
   );
 }
 
