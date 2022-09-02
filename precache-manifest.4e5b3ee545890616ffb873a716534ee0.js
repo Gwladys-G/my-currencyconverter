b2b2e93cef1c1aa6f55e082cda3c79c1c125f3fb
Gwladys Engels <gwladys.engels@gmail.com>
2022-09-02 16:21:40 -0400

    Updates

---
+++ precache-manifest.4e5b3ee545890616ffb873a716534ee0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "df621858010b7b8d7501",
+    "url": "/my-currencyconverter/static/css/main.c1751efa.chunk.css"
+  },
+  {
+    "revision": "df621858010b7b8d7501",
+    "url": "/my-currencyconverter/static/js/main.df621858.chunk.js"
+  },
+  {
+    "revision": "bb7509393a9253097fbb",
+    "url": "/my-currencyconverter/static/js/1.bb750939.chunk.js"
+  },
+  {
+    "revision": "dd23659e556eb4253ee3",
+    "url": "/my-currencyconverter/static/js/runtime~main.dd23659e.js"
+  },
+  {
+    "revision": "79f648820feb8bf1cb5bbe8f40874a1f",
+    "url": "/my-currencyconverter/index.html"
+  }
+];
