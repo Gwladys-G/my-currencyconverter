37b4a33fceadf627934ce00ec7a140d7e0d205f0
Gwladys Engels <gwladys.engels@gmail.com>
2022-09-02 09:58:16 -0400

    designed converter

--- src/Converter.jsx
+++ src/Converter.jsx
@@ -17,11 +17,11 @@ export default function Converter(props) {
   } = props
 
   return (
-    <>
-    <h1>Convert</h1>
-    <CurrencyRow currencyOptions={currencyOptions} selectedCurrency={selectedFromCurrency} onChangeCurrency={onChangeFromCurrency} amount={fromAmount} onChangeAmount={onChangeFromAmount}/>
-    <div className='equals'>=</div>
-    <CurrencyRow currencyOptions={currencyOptions} selectedCurrency={selectedToCurrency} onChangeCurrency={onChangeToCurrency} amount={toAmount} onChangeAmount={onChangeToAmount}/>
-  </>
+    <div className="converter">
+      <h1>Convert</h1>
+      <CurrencyRow currencyOptions={currencyOptions} selectedCurrency={selectedFromCurrency} onChangeCurrency={onChangeFromCurrency} amount={fromAmount} onChangeAmount={onChangeFromAmount}/>
+      <div className='equals'>=</div>
+      <CurrencyRow currencyOptions={currencyOptions} selectedCurrency={selectedToCurrency} onChangeCurrency={onChangeToCurrency} amount={toAmount} onChangeAmount={onChangeToAmount}/>
+    </div>
   )
 }
